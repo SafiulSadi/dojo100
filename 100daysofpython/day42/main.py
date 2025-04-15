be37607e459b959ca198e2faf805e9879052f490
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2025-04-15 17:05:48 +0600

    update

--- 100daysofpython/day42/main.py
+++ 100daysofpython/day42/main.py
@@ -12,10 +12,12 @@
 # for i in response():
 #     print(i)
 from google import genai
-promot = "Explain how robots works in a 50 words, response in json"
+prompt = "Explain how robots works in a 50 words, response in json"
+
+prompt = input("give a prompt: ")
 client = genai.Client(api_key="AIzaSyC4CITfd35eWv4HoDPnjJcsfFXBD6b1xzE")
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash", contents=promot
+    model="gemini-2.0-flash", contents=prompt
 )
 print(response.text)
