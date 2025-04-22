7211612c8eab0c7287bed6476c1bb5107d346437
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2025-04-22 18:06:22 +0600

    no progress has been made

--- 100daysofpython/day42/main.py
+++ 100daysofpython/day42/main.py
@@ -28,3 +28,4 @@ x = "the idea is to learn prompt engineering"
 print("hello, the idea is to use drone delivery and ai agent for education system")
 
 print("this agent can read emotion and then give feedback ")
+print("no progress has been made")
